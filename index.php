64097c61b027007adab64bc21da8f3c71c110736
AhmedAli-01 <15903693+AhmedAli-01@users.noreply.github.com>
2025-09-26 16:39:39 +0100

    Day 1: Laracast php for Beginners: added inline css, conditional statement

--- index.php
+++ index.php
@@ -1,20 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Demo</title>
-  </head>
-  <body>
-  <h1>
-    <?php
-    $greeting = "Hello";
 
-    //echo $greeting . " " . "theName!";
-    
-    //this is called refactoring
-    echo "$greeting Name!";
-    ?>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Demo</title>
+  <style>
+    body {
+      display: grid;
+      place-items: center;
+      height: 100vh;
+      margin: 0;
+      font-family: sans-serif;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php
+  $name = "Dark Matter";
+  $read = false;
+
+  if ($read) {
+    $message = "You have read $name";
+  } else {
+    $message = "You have NOT read $name";
+  }
+  ?>
+
+  <h1>
+    <?php echo $message ?>
   </h1>
-  </body>
+</body>
+
 </html>
